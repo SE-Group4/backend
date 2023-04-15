2c14c6474d2312913b291a5c64e364216040891c
Edward Edgar Arthur <108295488+edgararthur@users.noreply.github.com>
2023-04-15 22:44:10 +0000

    update consumers.py

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,38 +1,75 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
-import json
+# consumers.py
 
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
-
-        # join room
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
+        self.user = self.scope["user"]
+        await self.channel_layer.group_add(
+            f"user-{self.user.id}",
+            self.channel_name
+        )
         await self.accept()
 
-    async def disconnect(self, code):
-        # leave room
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            f"user-{self.user.id}",
+            self.channel_name
+        )
 
-    # receive message from websocket
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        data = json.loads(text_data)
+        message = data['message']
+        group_id = data.get('group_id')
+        recipient_id = data.get('recipient_id')
 
-        # send message to room
-        await self.channel_layer.group_send(self.room_group_name, {
-            'type': 'chat_message',
-            'message': message
-        })
+        if group_id:
+            await self.send_group_message(message, group_id)
+        elif recipient_id:
+            await self.send_private_message(message, recipient_id)
 
-    # Receive message from room
-    async def chat_message(self, event):
+    async def send_group_message(self, message, group_id):
+        group_name = f"group-{group_id}"
+        await self.channel_layer.group_send(
+            group_name,
+            {
+                'type': 'group.message',
+                'message': message,
+                'username': self.user.username
+            }
+        )
+
+    async def send_private_message(self, message, recipient_id):
+        recipient_name = f"user-{recipient_id}"
+        sender_name = f"user-{self.user.id}"
+        await self.channel_layer.send(
+            recipient_name,
+            {
+                'type': 'private.message',
+                'message': message,
+                'username': self.user.username,
+                'sender_id': self.user.id,
+                'recipient_id': recipient_id
+            }
+        )
+
+    async def group_message(self, event):
         message = event['message']
-
-        # send message to websocket
+        username = event['username']
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'username': username
         }))
 
+    async def private_message(self, event):
+        message = event['message']
+        username = event['username']
+        sender_id = event['sender_id']
+        recipient_id = event['recipient_id']
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+            'sender_id': sender_id,
+            'recipient_id': recipient_id
+        }))
